24d6ceb75080a21a86357fb4bc4550a14d97006e
Yri Davies <0xdavy@gmail.com>
2017-11-13 10:53:36 +0100

    Add git repos for plugins to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+# Clones git repos or otherwise fetches vim plugins
+
+REPOS=(
+    "https://github.com/sjl/badwolf.git"
+    "https://github.com/scrooloose/nerdtree.git" 
+    )
+
+cd vim/.vim/bundle
+for j in "${REPOS[@]}"
+do
+    git clone $j
+done
+cd ../../..
+
 # Executes stow commands for git'd dotfiles
 
-PROGRAMS=( "vim" "bash" )
+PROGRAMS=(
+    "vim"
+    "bash" 
+    )
 
 for i in "${PROGRAMS[@]}"
 do
