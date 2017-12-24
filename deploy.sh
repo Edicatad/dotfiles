0d764484b4533daab456450723eeaa7b25d94985
Yri Davies <0xdavy@gmail.com>
2017-12-24 18:24:55 +0100

    Improve deploy script
    Deploy script now has nicely formatted output. The script now also tries
    to pull repos if you already have a folder by that name - no more clone
    errors!

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,33 @@
 # Clones git repos or otherwise fetches vim plugins
 
 REPOS=(
-    "https://github.com/scrooloose/nerdtree.git" 
+    "https://github.com/scrooloose/nerdtree" 
     "https://github.com/sickill/vim-monokai"
     )
 
+echo "Cloning vim plugins from GitHub"
 cd vim/.vim/bundle
 for j in "${REPOS[@]}"
 do
-    git clone $j
+    REPONAME="${j##*/}"
+    if [ -s "${REPONAME}" ]; then
+        printf "  Pulling %-63s" "${REPONAME}"
+        cd "${REPONAME}"
+        OUTPUT="$(git pull 2>&1)"
+        RC="$?"
+        cd ..
+    else
+        printf "  Cloning %-63s" "$j"
+        OUTPUT="$(git clone $j 2>&1)"
+        RC="$?"
+    fi
+    if [ ${RC} -eq 0 ]; then
+        printf  "\e[1;32m[OK]\e[0m\n"
+    else
+        printf  "\e[1;31m[Error]\e[0m\n"
+        echo    "    exited with error code ${RC}"
+        printf  "    %s\n" "$(echo "${OUTPUT}" | sed '2,$s/^/    /g')"
+    fi
 done
 cd ../../..
 
@@ -21,7 +40,18 @@ PROGRAMS=(
     "tmux"
     )
 
+echo "Arguments supplied for GNU Stow: '-t ${HOME} ${1}'"
+echo 'Deploying configuration files:'
 for i in "${PROGRAMS[@]}"
 do
-    stow -t $HOME $1 $i
+    printf "  Deploying %-61s" "$i"
+    OUTPUT="$(stow -t $HOME $1 $i 2>&1)"
+    RC="$?"
+    if [ ${RC} -eq 0 ]; then
+        printf  "\e[1;32m[OK]\e[0m\n"
+    else
+        printf  "\e[1;31m[Error]\e[0m\n"
+        echo    "    exited with error code ${RC}"
+        printf  "    %s\n" "$(echo "${OUTPUT}" | sed '2,$s/^/    /g')"
+    fi
 done
