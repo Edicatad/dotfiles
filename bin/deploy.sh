47b2401fe4683570088131c593c23acbb676ea14
Yri Davies <0xdavy@gmail.com>
2018-02-24 11:29:53 +0100

    Rewrite large part of deploy script for ease of editing

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
 # Clones git repos or otherwise fetches vim plugins
 
-VIMPLUGINS=(
+VIM_PLUGINS=(
 "https://github.com/scrooloose/nerdtree" 
 "https://github.com/junegunn/fzf.vim"
 "https://github.com/chriskempson/base16-vim"
+"https://github.com/tpope/vim-surround"
 )
 
-STOWPROGRAMS=(
+STOW_PROGRAMS=(
 "vim"
 "bash" 
 "tmux"
 "mutt"
 "xresources"
+"newsbeuter"
 )
 
-CLITOOLS=(
+CLI_TOOLS=(
 "https://github.com/chriskempson/base16-shell"
 )
 
-COLOURSCHEME=(
+COLOUR_SCHEME=(
 "https://github.com/nicodebo/base16-fzf"
 )
 
 #=====
-#  Function
-#
+#  Script variables
+#==
+preset_answer=''
+stow_args=''
+
+#=====
+#  Functions
+#==
+
+#=====
+#  Reads user input.  This ensures we're using the same parameters
+#  for every input query, and it makes the -yn flags possible.
+#  
+#  Params
+#  $1: Query text
+#==
+function _read {
+    if [ -z "${preset_answer}" ]; then
+        read -t 3 -p "$1" answer
+    else
+        answer="${preset_answer}"
+    fi
+}
+
+#=====
 #  Loops through an array of git repositories looking for a folder
 #  of the same name.  If it finds one, it pulls that repository, 
 #  and if not it clones it.
@@ -34,28 +59,35 @@ COLOURSCHEME=(
 #  $1: Name of an array containing git repository urls
 #==
 function _clone_or_pull_repos {
-    arrayname=$1[@]
-    array=("${!arrayname}")
+    array_name=$1[@]
+    array=("${!array_name}")
     for j in "${array[@]}"
     do
-        REPONAME="${j##*/}"
-        if [ -s "${REPONAME}" ]; then
-            printf "  Pulling %-63s" "${REPONAME}"
-            cd "${REPONAME}"
-            OUTPUT="$(git pull 2>&1)"
-            RC="$?"
+        repo_name="${j##*/}"
+        if [ -s "${repo_name}" ]; then
+            printf "  Pulling %-63s" "${repo_name}"
+            cd "${repo_name}"
+            output="$(git pull 2>&1)"
+            exit_status="$?"
             cd ..
         else
-            printf "  Cloning %-63s" "$j"
-            OUTPUT="$(git clone --depth 1 $j 2>&1)"
-            RC="$?"
+            echo "New source found in $1: ${repo_name}"
+            _read "Do you want to clone the repository [yN]"
+            case ${answer:0:1} in
+                y|Y )
+                    printf "  Cloning %-63s" "$j"
+                    output="$(git clone --depth 1 $j 2>&1)"
+                    ;;
+                * ) echo ;;
+            esac
+            exit_status="$?"
         fi
-        if [ ${RC} -eq 0 ]; then
+        if [ ${exit_status} -eq 0 ]; then
             printf  "\e[1;32m[OK]\e[0m\n"
         else
             printf  "\e[1;31m[Error]\e[0m\n"
-            echo    "    exited with error code ${RC}"
-            printf  "    %s\n" "$(echo "${OUTPUT}" | sed '2,$s/^/    /g')"
+            echo    "    exited with error code ${exit_status}"
+            printf  "    %s\n" "$(echo "${output}" | sed '2,$s/^/    /g')"
         fi
     done
 }
@@ -68,8 +100,8 @@ function _clone_or_pull_repos {
 #  $2: Link target
 #==
 function _link {
-    unamestr="$(uname -s)"
-    case "${unamestr}" in
+    uname_str="$(uname -s)"
+    case "${uname_str}" in
         Linux*)
             ln -sfn $1 $2
             ;;
@@ -79,32 +111,62 @@ function _link {
     esac
 }
 
+#=====
+#  Script
+#==
+
+#=====
+#  Parse command line flags
+#==
+while getopts ":ynr" option; do
+    case "${option}" in
+        y) preset_answer='y' ;;
+        n) preset_answer='n' ;;
+        r) stow_args="${stow_args} -R" ;;
+        \?) echo "Unknown flag '${OPTARG}'" ;;
+    esac
+done
+
 echo "Cloning vim plugins using git"
 cd vim/.vim/bundle
-_clone_or_pull_repos VIMPLUGINS
+_clone_or_pull_repos VIM_PLUGINS
 cd ../../..
 
 # Executes stow commands for git'd dotfiles
 
-echo "Arguments supplied for GNU Stow: '-t ${HOME} ${1}'"
+echo "Arguments supplied for GNU Stow: '-t ${HOME}${stow_args}'"
 echo 'Deploying configuration files:'
-for i in "${STOWPROGRAMS[@]}"
+for i in "${STOW_PROGRAMS[@]}"
 do
     printf "  Deploying %-61s" "$i"
-    OUTPUT="$(stow -t $HOME $1 $i 2>&1)"
-    RC="$?"
-    if [ ${RC} -eq 0 ]; then
+    output="$(stow -t $HOME $1 $i 2>&1)"
+    exit_status="$?"
+    if [ ${exit_status} -eq 0 ]; then
         printf  "\e[1;32m[OK]\e[0m\n"
     else
         printf  "\e[1;31m[Error]\e[0m\n"
-        echo    "    exited with error code ${RC}"
-        printf  "    %s\n" "$(echo "${OUTPUT}" | sed '2,$s/^/    /g')"
+        echo    "    exited with error code ${exit_status}"
+        printf  "    %s\n" "$(echo "${output}" | sed '2,$s/^/    /g')"
     fi
 done
 
+# Symlink tools
+echo "Cloning tools using git"
+cd tools
+_clone_or_pull_repos CLI_TOOLS
+cd ..
+_link "$(pwd)/tools" ~/.dotfiles-tools
+
+# Fetch colour scheme
+echo "Cloning colourscheme using git"
+cd colour
+_clone_or_pull_repos COLOUR_SCHEME
+cd ..
+_link "$(pwd)/colour" ~/.dotfiles-colours
+
 # Symlink userdata
 if [ -d ~/.dotfiles-userdata ]; then
-    read -t 3 -p "Do you want to overwrite userdata? [yN]" answer
+    _read "Do you want to overwrite userdata? [yN]"
     case ${answer:0:1} in
         y|Y )
             echo "Overwriting userdata"
@@ -117,32 +179,4 @@ else
     # ln -s is unambiguous so doesn't need a wrapper function
     ln -s "$(pwd)/userdata" ~/.dotfiles-userdata
 fi
-
-# Symlink tools
-read -t 3 -p "Do you want to install the basic tools? [Yn]" answer
-case ${answer:0:1} in
-    n|N ) echo ;;
-    * ) 
-        echo
-        echo "Cloning tools using git"
-        cd tools
-        _clone_or_pull_repos CLITOOLS
-        cd ..
-        _link "$(pwd)/tools" ~/.dotfiles-tools
-        ;;
-esac
-
-# Fetch colour scheme
-read -t 3 -p "Do you want to install the current colour scheme? [Yn]" answer
-case ${answer:0:1} in
-    n|N) echo ;;
-    *)
-        echo
-        echo "Cloning colourscheme using git"
-        cd colour
-        _clone_or_pull_repos COLOURSCHEME
-        cd ..
-        _link "$(pwd)/colour" ~/.dotfiles-colours
-        ;;
-esac
 
