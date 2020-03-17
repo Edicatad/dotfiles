812a92deea0b6ef1f74824a22280e3db76388e8d
Yri Davies <0xdavy@gmail.com>
2020-03-17 11:01:01 +0100

    Update tmux config to work with new syntax

--- tmux/.tmux/scripts/on-switch-window.sh
+++ tmux/.tmux/scripts/on-switch-window.sh
@@ -3,8 +3,8 @@ WINDOWNAME=`tmux display-message -p '#W'`
 if [ "$WINDOWNAME" = "ssh" ]
 then
     # We're in a ssh window!
-    tmux set -g status-bg colour9
+    tmux set -g status-style bg=colour9
 else
     # We're not in a ssh window, so apply default formatting
-    tmux set -g status-bg colour18
+    tmux set -g status-style bg=colour18
 fi
